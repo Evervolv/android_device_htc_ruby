87ab9c7578faa3b65ff79b31fd486ef4bb87d990
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2012-03-14 20:35:56 -0500

    ruby: hot reboot / dont override buildid
    Change-Id: Ie5d7f23d37ec9284f65672b780b465a27a7bfe84

--- ev.mk
+++ ev.mk
@@ -4,18 +4,15 @@ $(call inherit-product, device/htc/ruby/full_ruby.mk)
 # Inherit some common evervolv stuff.
 $(call inherit-product, vendor/ev/config/common_full_phone.mk)
 
-# Inherit some evervolv gsm stuff.
-$(call inherit-product, vendor/ev/config/gsm.mk)
-
 #
 # Setup device specific product configuration.
 #
-PRODUCT_NAME := ev_ruby
-PRODUCT_BRAND := HTC
-PRODUCT_DEVICE := ruby
-PRODUCT_MODEL := Amaze 4g
+PRODUCT_NAME    := ev_ruby
+PRODUCT_BRAND   := HTC
+PRODUCT_DEVICE  := ruby
+PRODUCT_MODEL   := Amaze 4g
 PRODUCT_MANUFACTURER := HTC
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_ruby BUILD_ID=GRJ22 BUILD_FINGERPRINT=tmous/htc_ruby/ruby:2.3.4/GRJ22/192596.3:user/release-keys PRIVATE_BUILD_DESC="1.43.531.3 CL83920 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_ruby BUILD_FINGERPRINT=tmous/htc_ruby/ruby:2.3.4/GRJ22/192596.3:user/release-keys PRIVATE_BUILD_DESC="1.43.531.3 CL83920 release-keys"
 
 # Set up the product codename, build version & MOTD.
 PRODUCT_CODENAME := Iaceo
@@ -30,3 +27,6 @@ PRODUCT_PACKAGES += \
 # Copy compatible bootanimation
 PRODUCT_COPY_FILES +=  \
     vendor/ev/prebuilt/qhd/media/bootanimation.zip:system/media/bootanimation.zip
+
+# Hot reboot
+PRODUCT_PACKAGE_OVERLAYS += vendor/ev/overlay/hot_reboot
