080857abfa19cc15af2768df78c27787ee9eadd2
Nick Reuter <nreuter85@gmail.com>
2015-08-18 21:36:36 -0500

    ruby: update for vendor changes
    Change-Id: If571ccf326f3e6423f6b116498c6980a76df74fd

--- ev.mk
+++ ev.mk
@@ -1,12 +1,32 @@
-# Inherit AOSP device configuration for ruby.
+#
+# Copyright (C) 2013 The Evervolv Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Call this first so apn list is actually copied
+$(call inherit-product, $(SRC_EVERVOLV_DIR)/config/apns.mk)
+
+# Product makefile
 $(call inherit-product, device/htc/ruby/device.mk)
 
 # Inherit some common evervolv stuff.
-$(call inherit-product, vendor/ev/config/gsm.mk)
-$(call inherit-product, vendor/ev/config/common_full_phone.mk)
+$(call inherit-product, $(SRC_EVERVOLV_DIR)/config/common_full_phone.mk)
 
-# Call gps Xtra server configuration
-$(call inherit-product, device/common/gps/gps_us.mk)
+# Pull all dictionaries
+$(call inherit-product, $(SRC_EVERVOLV_DIR)/config/dictionaries/intl.mk)
+
+# Bootanimation
+BOOT_ANIMATION_SIZE := qhd
 
 #
 # Setup device specific product configuration.
@@ -21,11 +41,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_ruby BUILD_FINGERPRINT=tmous/ht
 # Set up the product codename, build version & MOTD.
 PRODUCT_CODENAME := Iaceo
 
-PRODUCT_MOTD :="\n\n\n--------------------MESSAGE---------------------\nThank you for choosing Evervolv for your HTC Amaze 4g\n------------------------------------------------\n"
+PRODUCT_MOTD :="\n\n\n--------------------MESSAGE---------------------\nThank you for choosing Evervolv for your HTC Amaze 4G\nPlease visit us at \#evervolv on irc.freenode.net\nFollow @preludedrew for the latest Evervolv updates\nGet the latest rom at evervolv.com\n------------------------------------------------\n"
 
-# Copy compatible bootanimation
-PRODUCT_COPY_FILES +=  \
-    vendor/ev/prebuilt/qhd/media/bootanimation.zip:system/media/bootanimation.zip
 
-# Hot reboot
-PRODUCT_PACKAGE_OVERLAYS += vendor/ev/overlay/hot_reboot
